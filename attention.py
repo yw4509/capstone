3e98606d6b9b6265a4ec4e4cf117ef556864cfb6
yw4509 <yw4509@nyu.edu>
2020-12-08 09:48:49 -0500

    add attention

--- attention.py
+++ attention.py
@@ -88,7 +88,7 @@ class WikiDataset():
                         ans_tokenized,                      # Sentence to encode.
                         add_special_tokens = True, # Add '[CLS]' and '[SEP]'
                         truncation=True,
-                        max_length = 64,           # Pad & truncate all sentences.
+                        max_length = 35,           # Pad & truncate all sentences.
                         pad_to_max_length = True,
                         return_attention_mask = True,   # Construct attn. masks.
                         return_tensors = 'pt',     # Return pytorch tensors.
